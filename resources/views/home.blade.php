dda6200b3b647c29615b0386c8c24d45b5004880
Josh Kalsi <josh.kalsi@gmail.com>
2018-11-08 16:58:41 +0000

    Add Bootstrap and move base layout to separate file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,36 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TODO</title>
-</head>
-<body>
+@extends('layouts.app')
 
-    <h1>TODO List</h1>
+@section('content')
 
-    <form action="/api/tasks" method="post" onsubmit="return false">
-        <input type="text" name="name" placeholder="New Task">
-        <input type="date" name="due_at">
-        <input type="submit">
-    </form>
 
-    @foreach ($tasks as $task)
-        <div class="task">
-            <p>Name: {{ $task->name }}</p>
-            <p>Due: {{ $task->due_at }}</p>
-            <p>Created: {{ $task->created_at }}</p>
+    <header class="container text-center">
+        <h1>TODO List</h1>
+    </header>
+
+    <div class="container text-center">
+        <div class="row">
+            <div class="col">
+                @foreach ($tasks as $task)
+                <div class="task">
+                <p>Name: {{ $task->name }}</p>
+                <p>Due: {{ $task->due_at }}</p>
+                <p>Created: {{ $task->created_at }}</p>
+                <form action="/api/tasks/{{ $task ->id }}" method="post">
+                {{method_field('DELETE')}}
+                <button class="btn btn-danger">Delete</button>
+                </form>
+                </div>
+                @endforeach
+            </div>
+            <div class="col">
+                <form action="/api/tasks" method="post">
+                <div class="form-group">
+
+                <input type="text" name="name" placeholder="New Task">
+                </div>
+                <input type="date" name="due_at">
+                <input type="submit">
+                </form>
+            </div>
         </div>
-    @endforeach
-
-</body>
-</html>
+    </div>
