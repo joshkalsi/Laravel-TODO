2a6a9ca4c387dde1cf7678cb875ddd57d33d745d
Josh Kalsi <josh.kalsi@gmail.com>
2018-11-09 11:53:27 +0000

    Refactor blade files again

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,46 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
-@section('content')
-
-
-    <header class="container text-center">
-        <h1>TODO List</h1>
-    </header>
-
-    <div class="container text-center">
-        <div class="row">
-            <div class="col">
-                @foreach ($tasks as $task)
-                <div class="task">
-                <p>Name: {{ $task->name }}</p>
-                <p>Due: {{ $task->due_at }}</p>
-                <p>Created: {{ $task->created_at }}</p>
-                <form action="/api/tasks/{{ $task ->id }}" method="post">
-                {{method_field('DELETE')}}
-                <button class="btn btn-danger">Delete</button>
-                </form>
+@section('add-task')
+    <div class="container">
+        <form action="/api/tasks" method="post">
+            <div class="form-group row">
+                <label class="col col-form-label text-center" for="task-name">Task</label>
+                <div class="col-6">
+                    <input class="form-control text-center" type="text" name="name" id="task-name" placeholder="New Task" required>
                 </div>
-                @endforeach
-            </div>
-            <div class="col">
-                <form action="/api/tasks" method="post">
-                <div class="form-group">
-
-                <input type="text" name="name" placeholder="New Task">
+                <label class="col col-form-label text-center" for="due_at">Date Due</label>
+                <div class="col">
+                    <input class="form-control text-center" type="date" name="due_at" id="due_at" required>
                 </div>
-                <input type="date" name="due_at">
-                <input type="submit">
-                </form>
             </div>
-        </div>
+            <div class="col text-center">
+                <input class="btn btn-primary" type="submit" value="Add Task">
+            </div>
+        </form>
     </div>
+@stop
+
+@section('task-list')
+    <div class="container p-3">
+        <ul class="list-group">
+            @foreach ($tasks as $task)
+                <div class="list-group-item">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h3 class={{ "" . $task->due_at->isPast() ? "text-danger" : ""}}>{{ $task->name }}</h3>
+                        </div>
+                        <div class="col text-center">
+                            <p class={{ $task->due_at->isPast() ? "text-danger" : ""}}>Due {{ \Carbon\Carbon::parse($task->due_at)->format('d/m/Y') }}</p>
+                            <form action="/api/tasks/{{ $task ->id }}" method="post">
+                                {{method_field('DELETE')}}
+                                <button class="btn btn-danger">Delete</button>
+                            </form>
+                        </div>
+                    </div>
+
+                </div>
+            @endforeach
+        </ul>
+    </div>
+@stop
+
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TODO</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <style>
+        input[type=date]::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            display: none;
+        }
+        header {
+            border-bottom-left-radius: 1em;
+            border-bottom-right-radius: 1em;
+        }
+    </style>
+
+    <script>
+        const markDone = (id) => {
+            document.getElementById('')
+        }
+    </script>
+</head>
+<body>
+
+<header class="container text-center p-2 bg-light">
+    <h1>TODO List</h1>
+    @yield('nav')
+    @yield('add-task')
+</header>
+
+
+
+@yield('task-list')
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+</body>
+</html>
